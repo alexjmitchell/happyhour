da330de35df5eeb645c2c38ac17f581789a83d45
Punchcode <lazaruslyon42@gmail.com>
2019-12-06 09:29:03 -0800

    Footer buildr

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,18 +1,18 @@
 import React from "react"
 import "../styles/Maincss.css"
 import Slider from "../components/Slider.js"
-import Header from "./Header"
+// import Header from "./Header"
 import Footer from "./Footer"
-import MainBaner from "./Main-Baner"
+import MainBanner from "./Main-Banner"
+import GoogleMaps1 from "./GoogleMaps1"
 
 function Main() {
   return (
     <div>
 
-      <Header />
-      <MainBaner />
+      <MainBanner />
       <Slider />
-      
+      <GoogleMaps1 />
       <Footer />
    
     </div>
