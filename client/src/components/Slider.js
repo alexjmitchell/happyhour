1b89fcdd8a883c23b01f432ce008225413022103
Danilo Nikolic <nikolicdanilo1410@gmail.com>
2019-12-13 10:25:13 -0800

    Filtering

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -5,48 +5,25 @@ import Icon from "../lib/Icon"
 import { useUsers, useCompanies, useLiked } from "../hooks"
 
 import Radium, { Style, StyleRoot } from "radium"
-import { set } from "date-fns"
 
 function Slider() {
-  const { filteredUsers, filterBars, users } = useUsers()
-
-  // const { companyname } = useCompanies()
-  // const { liked } = useLiked()
-
-  const [search, setSearch] = useState("")
+  const { users, filter } = useUsers() //all the companies
+  const { companyname } = useCompanies()
+  const { liked } = useLiked()
   const [val, setVal] = useState("")
-
   var d = new Date()
   var hr = d.getHours()
-
-  const fn = function() {}
+  const fn = function() {} // slider library
 
   function handleClick(e, liked) {
     e.preventDefault()
     // props.history.push("/CompanyPage/" + liked)   je ako hocemo da prenesemo data na bilo koju komponentu . u ovom slucaju je CompanyPage. a + liked je data koju prenosimo tamo
   }
 
-  function handleSearch(e) {
-    setSearch(e.target.value)
-    filterBars({
-      search,
-      val
-    })
-  }
+  console.log(hr, "eee")
 
-  function handleVal(e) {
-    setVal(e.target.value)
-    filterBars({
-      search,
-      val
-    })
-  }
-
-  // const newArray = users.filter(p => p.starthour == val)
-  // const newSearch = users.filter(c => c.companyname == search)
-
-  // console.log(newSearch, "aaa")
-  // console.log(newArray, "ppp")
+  const newArray = users.filter(p => p.starthour >= val)
+  console.log(newArray)
   return (
     <div className="sliderW">
       {/* <p>
@@ -59,7 +36,9 @@ function Slider() {
 
       {/* // onChange={e => filter(e.target.value)}  */}
 
-      <select onChange={handleVal}>
+      {/* // ovde prosledjujemo e.target.value u setVal() i to je sad u stvari val  */}
+      {/* // e.target.value je u stvari value bilo koja od 00 do 23 koji prenosimo preko setVal u val i komperujemo je sa starthour u filter i prenosimo u newArray */}
+      <select onChange={e => setVal(e.target.value)}>
         <option value="default"> Select</option>
         <option value="00">12:00 am</option>
         <option value="01">01:00 am</option>
@@ -86,12 +65,8 @@ function Slider() {
         <option value="22">10:00 pm</option>
         <option value="23">11:00 pm</option>
       </select>
-
+      {/* // ovde prosledjujemo hr u setVal() i to je sad u stvari val  koji je jednak sa data koji komperujemo u ovom slucaju starthour gore u filter */}
       <button onClick={e => setVal(hr)}>happy hour now</button>
-
-      <form>
-        <input type="text" onChange={handleSearch} />
-      </form>
 
       <div>
         <Coverflow
@@ -100,6 +75,8 @@ function Slider() {
           displayQuantityOfSide={2.5}
           navigation={false}
           enableScroll={false}
+          // enableHeading={true}
+          // infiniteScroll={true}
           clickable={true}
           active={0}
 
@@ -115,10 +92,10 @@ function Slider() {
           // }}
         >
           {/* // ternary operator // {newArray.length === 0 ? "" : ""} // if and else na istoj liniji u ovom slucaju je prvi map ili drugi */}
-          {filteredUsers.length === 0
+          {newArray.length === 0
             ? users.map((user, i) => (
                 <img
-                  key={"imageee" + i}
+                  key={i}
                   className="slidePics"
                   src={user.picture}
                   height={450}
@@ -128,11 +105,10 @@ function Slider() {
                     </a>
                   }
                 />
-                // <Link to="/">Click Here</Link>
               ))
-            : filteredUsers.map((user, i) => (
+            : newArray.map((user, i) => (
                 <img
-                  key={"imageeeee" + i}
+                  key={i}
                   className="slidePics"
                   src={user.picture}
                   height={450}
@@ -149,3 +125,156 @@ function Slider() {
   )
 }
 export default Slider
+
+// import React, { useState } from "react"
+// import Coverflow from "react-coverflow"
+// import { Link } from "react-router-dom"
+// import Icon from "../lib/Icon"
+// import { useUsers, useCompanies, useLiked } from "../hooks"
+
+// import Radium, { Style, StyleRoot } from "radium"
+// import { set } from "date-fns"
+
+// function Slider() {
+//   const { filteredUsers, filterBars, users } = useUsers()
+
+//   // const { companyname } = useCompanies()
+//   // const { liked } = useLiked()
+
+//   const [search, setSearch] = useState("")
+//   const [val, setVal] = useState("")
+
+//   var d = new Date()
+//   var hr = d.getHours()
+
+//   const fn = function() {}
+
+//   function handleClick(e, liked) {
+//     e.preventDefault()
+//     // props.history.push("/CompanyPage/" + liked)   je ako hocemo da prenesemo data na bilo koju komponentu . u ovom slucaju je CompanyPage. a + liked je data koju prenosimo tamo
+//   }
+
+//   function handleSearch(e) {
+//     setSearch(e.target.value)
+//     filterBars({
+//       search,
+//       val
+//     })
+//   }
+
+//   function handleVal(e) {
+//     setVal(e.target.value)
+//     filterBars({
+//       search,
+//       val
+//     })
+//   }
+
+//   // const newArray = users.filter(p => p.starthour == val)
+//   // const newSearch = users.filter(c => c.companyname == search)
+
+//   // console.log(newSearch, "aaa")
+//   // console.log(newArray, "ppp")
+//   return (
+//     <div className="sliderW">
+//       {/* <p>
+//         Check Your Liked List Here:{" "}
+//         <Link to="/liked">
+//           {" "}
+//           <Icon icon="heart" />
+//         </Link>
+//       </p> */}
+
+//       {/* // onChange={e => filter(e.target.value)}  */}
+
+//       <select onChange={handleVal}>
+//         <option value="default"> Select</option>
+//         <option value="00">12:00 am</option>
+//         <option value="01">01:00 am</option>
+//         <option value="02">02:00 am</option>
+//         <option value="03">03:00 am</option>
+//         <option value="04">04:00 am</option>
+//         <option value="05">05:00 am</option>
+//         <option value="06">06:00 am</option>
+//         <option value="07">07:00 am</option>
+//         <option value="08">08:00 am</option>
+//         <option value="09">09:00 am</option>
+//         <option value="10">10:00 am</option>
+//         <option value="11">11:00 am</option>
+//         <option value="12">12:00 pm</option>
+//         <option value="13">01:00 pm</option>
+//         <option value="14">02:00 pm</option>
+//         <option value="15">03:00 pm</option>
+//         <option value="16">04:00 pm</option>
+//         <option value="17">05:00 pm</option>
+//         <option value="18">06:00 pm</option>
+//         <option value="19">07:00 pm</option>
+//         <option value="20">08:00 pm</option>
+//         <option value="21">09:00 pm</option>
+//         <option value="22">10:00 pm</option>
+//         <option value="23">11:00 pm</option>
+//       </select>
+
+//       <button onClick={e => setVal(hr)}>happy hour now</button>
+
+//       <form>
+//         <input type="text" onChange={handleSearch} />
+//       </form>
+
+//       <div>
+//         <Coverflow
+//           width="960"
+//           height="1000"
+//           displayQuantityOfSide={2.5}
+//           navigation={false}
+//           enableScroll={false}
+//           clickable={true}
+//           active={0}
+
+//           // media={{
+//           //   "@media (max-width: 900px)": {
+//           //     // width: "600px"
+//           //     // height: "300px"
+//           //   },
+//           //   "@media (min-width: 900px)": {
+//           //     // width: "960px"
+//           //     // height: "800px"
+//           //   }
+//           // }}
+//         >
+//           {/* // ternary operator // {newArray.length === 0 ? "" : ""} // if and else na istoj liniji u ovom slucaju je prvi map ili drugi */}
+//           {filteredUsers.length === 0
+//             ? users.map((user, i) => (
+//                 <img
+//                   key={"imageee" + i}
+//                   className="slidePics"
+//                   src={user.picture}
+//                   height={450}
+//                   alt={
+//                     <a className="sliderImg" href={user.website}>
+//                       {user.companyname}
+//                     </a>
+//                   }
+//                 />
+//                 // <Link to="/">Click Here</Link>
+//               ))
+//             : filteredUsers.map((user, i) => (
+//                 <img
+//                   key={"imageeeee" + i}
+//                   className="slidePics"
+//                   src={user.picture}
+//                   height={450}
+//                   alt={
+//                     <a className="sliderImg" href={user.website}>
+//                       {user.companyname}
+//                     </a>
+//                   }
+//                 />
+//               ))}
+//         </Coverflow>
+//       </div>
+//     </div>
+//   )
+
+// }
+// export default Slider
--- client/src/redux/ducks/users/index.js
+++ client/src/redux/ducks/users/index.js
@@ -8,56 +8,22 @@ const FILTER_PIC = "filter / FILTER_PIC"
 
 // initial state
 const initialState = {
-  users: [],
-  filteredUsers: []
+  users: []
 }
 
 // reducer
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_PICTURES:
-      return { ...state, users: action.payload, filteredUsers: action.payload }
+      return { ...state, users: action.payload }
     case FILTER_PIC:
       return {
         ...state,
-        filteredUsers: filterCompanies(action.payload, state.users)
+        users: state.users.filter(p => p.starthour == action.filter1)
       }
     default:
       return state
   }
-}
-
-function filterCompanies(filterobj, users) {
-  let { search, val } = filterobj
-
-  if (!search) {
-    search = ""
-  }
-
-  if (!val) {
-    val = ""
-  }
-  // bar.companyname
-  // bar.starthoure
-  // bar.endhour
-
-  let f = users.filter(bar => {
-    if (search && val) {
-      return (
-        bar.companyname == search && val > bar.starthour && val < bar.endhour
-      )
-    }
-
-    if (search && !val) {
-      return bar.companyname == search
-    }
-
-    if (val && !search) {
-      return val > bar.starthour && val < bar.endhour
-    }
-  })
-
-  return f
 }
 
 // action creators
@@ -84,10 +50,10 @@ function sendSubscrib(email) {
   }
 }
 
-const filterBarsAction = filterobj => {
+const filterHours = filter1 => {
   return {
     type: FILTER_PIC,
-    payload: filterobj
+    filter1: filter1
   }
 }
 
@@ -95,11 +61,8 @@ const filterBarsAction = filterobj => {
 
 export function useUsers() {
   const users = useSelector(appState => appState.userState.users)
-  const filteredUsers = useSelector(
-    appState => appState.userState.filteredUsers
-  )
   const dispatch = useDispatch()
-  const filterBars = filteredobj => dispatch(filterBarsAction(filteredobj))
+  const filter = filt1 => dispatch(filterHours(filt1))
   const get = () => dispatch(getPic())
   const sendF = (message, email, name) => {
     return dispatch(sendFeedback(message, email, name))
@@ -113,5 +76,122 @@ export function useUsers() {
     get()
   }, [dispatch])
 
-  return { users, sendF, sendSubs, filterBars, filteredUsers }
+  return { users, sendF, sendSubs, filter }
 }
+// import { useEffect } from "react"
+// import { useSelector, useDispatch } from "react-redux"
+// import axios from "axios"
+
+// // action definitions
+// const GET_PICTURES = "users/GET_PICTURES"
+// const FILTER_PIC = "filter / FILTER_PIC"
+
+// // initial state
+// const initialState = {
+//   users: [],
+//   filteredUsers: []
+// }
+
+// // reducer
+// export default (state = initialState, action) => {
+//   switch (action.type) {
+//     case GET_PICTURES:
+//       return { ...state, users: action.payload, filteredUsers: action.payload }
+//     case FILTER_PIC:
+//       return {
+//         ...state,
+//         filteredUsers: filterCompanies(action.payload, state.users)
+//       }
+//     default:
+//       return state
+//   }
+// }
+
+// function filterCompanies(filterobj, users) {
+//   let { search, val } = filterobj
+
+//   if (!search) {
+//     search = ""
+//   }
+
+//   if (!val) {
+//     val = ""
+//   }
+//   // bar.companyname
+//   // bar.starthoure
+//   // bar.endhour
+
+//   let f = users.filter(bar => {
+//     if (search && val) {
+//       return (
+//         bar.companyname == search && val > bar.starthour && val < bar.endhour
+//       )
+//     }
+
+//     if (search && !val) {
+//       return bar.companyname == search
+//     }
+
+//     if (val && !search) {
+//       return val > bar.starthour && val < bar.endhour
+//     }
+//   })
+
+//   return f
+// }
+
+// // action creators
+// const getPic = () => {
+//   return dispatch => {
+//     axios.get("/companies").then(resp => {
+//       dispatch({
+//         type: GET_PICTURES,
+//         payload: resp.data
+//       })
+//     })
+//   }
+// }
+
+// function sendFeedback(message, email, name) {
+//   return dispatch => {
+//     axios.post("/feedback", { message, email, name }).then(resp => {})
+//   }
+// }
+
+// function sendSubscrib(email) {
+//   return dispatch => {
+//     axios.post("/subscribers", { email }).then(resp => {})
+//   }
+// }
+
+// const filterBarsAction = filterobj => {
+//   return {
+//     type: FILTER_PIC,
+//     payload: filterobj
+//   }
+// }
+
+// // custom hooks
+
+// export function useUsers() {
+//   const users = useSelector(appState => appState.userState.users)
+//   const filteredUsers = useSelector(
+//     appState => appState.userState.filteredUsers
+//   )
+//   const dispatch = useDispatch()
+//   const filterBars = filteredobj => dispatch(filterBarsAction(filteredobj))
+//   const get = () => dispatch(getPic())
+//   const sendF = (message, email, name) => {
+//     return dispatch(sendFeedback(message, email, name))
+//   }
+
+//   const sendSubs = email => {
+//     return dispatch(sendSubscrib(email))
+//   }
+
+//   useEffect(() => {
+//     get()
+//   }, [dispatch])
+
+//   return { users, sendF, sendSubs, filterBars, filteredUsers }
+// }
